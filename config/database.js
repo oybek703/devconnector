5ff528a83811708c174d7462a107805023b9753c
oybek703 <hhoybek@gmail.com>
2020-11-07 22:17:02 +0500

    express and mongodb setup completed

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const MONGO_URI = config.get('MONGO_URI');
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+    } catch (e) {
+        console.error(e.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectToDatabase;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const connectToDatabase  = require('./config/database');
 const app = express();
 
-app.get('/', (req, res) => res.send('API is running!'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.NODE_ENV || 5000;
+connectToDatabase().then(() => console.log('Connected to MongoDB...'));
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}...`)});
